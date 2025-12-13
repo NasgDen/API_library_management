dbe8e1f399ec67498edfe88f3acb049f57e29b5a
Denis Safonov <denissafonov_85@mail.ru>
2025-12-13 21:18:15 +0500

    Создана модель Author и Book

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'users',
+    "users",
+    "library_management",
 ]
 
 MIDDLEWARE = [
@@ -99,4 +100,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "users.User"
-
