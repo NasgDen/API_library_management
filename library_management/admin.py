6b28148fb57e7c5aad3a2598b5b8f7169c041549
Denis Safonov <denissafonov_85@mail.ru>
2025-12-14 21:21:40 +0500

    В админке зарегестрированы модели Book и Author

--- library_management/admin.py
+++ library_management/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from library_management.models import Book, Author
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display =(
+        "title",
+        "author",
+        "year",
+        "owner",
+        "date_create",
+        "date_update"
+    )
+    list_filter = ("title",)
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display =(
+        "first_name",
+        "last_name",
+        "patronymic",
+        "owner",
+        "date_create",
+        "date_update"
+    )
+    list_filter = ("last_name",)
